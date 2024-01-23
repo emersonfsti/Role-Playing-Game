7d997b75ebd34064b338dafafb203c4f5ee0495f
emersonfsti <emersonfsti@gmail.com>
2024-01-23 20:36:56 -0300

    commit20240123001

--- script.js
+++ script.js
@@ -19,12 +19,27 @@ const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
 // initialize buttons
-button1.onClick = goStore;
+button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
+function goTown() {
+  button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
+  text.innerText = "You enter the store.";
+}
 function goStore() {
   button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
+  text.innerText = "You enter the store.";
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
 }
 
 function goCave() {
@@ -33,3 +48,7 @@ function goCave() {
 function fightDragon() {
   console.log("Fighting dragon.");
 }
+
+function buyHealth() {}
+
+function buyWeapon() {}
