658ce9d43d1ccbb14641f8f0f10b88ab155553ca
emersonfsti <emersonfsti@gmail.com>
2024-01-22 21:28:28 -0300

    nova005

--- script.js
+++ script.js
@@ -9,3 +9,27 @@ let inventory = ["stick", "dagger", "sword"];
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
+
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+// initialize buttons
+button1.onClick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+function goStore() {
+  button1.innerText = "Buy 10 health (10 gold)";
+}
+
+function goCave() {
+  console.log("Going to cave.");
+}
+function fightDragon() {
+  console.log("Fighting dragon.");
+}
