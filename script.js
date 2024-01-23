480803aa51b890bed6782964a0dfba26524ba2f0
emersonfsti <emersonfsti@gmail.com>
2024-01-22 21:13:44 -0300

    nova04

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick", "dagger", "sword"];
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
